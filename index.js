fa405876146726fa6d58cfdeb0524aeb8824373a
yigasda <143778990+yigasda@users.noreply.github.com>
2026-05-16 08:40:09 +0900

    Change header text for dynamic sprite settings

--- index.js
+++ index.js
@@ -1244,7 +1244,7 @@ function createSettingsPanel() {
     <div id="dynamic-sprites-settings" class="dynamic-sprites-settings">
         <div class="inline-drawer">
             <div class="inline-drawer-toggle inline-drawer-header">
-                <b>🎭 Dynamic Sprite Emotion</b>
+                <b>Dynamic Sprite</b>
                 <div class="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
             </div>
             <div class="inline-drawer-content">
